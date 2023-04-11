cde9e1a93f2e5ef8c800dfa8674c6c9b327c3e2d
ALVIAN_085 <114425441+ALVIANRML@users.noreply.github.com>
2023-04-11 21:59:09 +0700

    Add files via upload

---
+++ for_loop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+cout << "Awal program \n";
+
+for ( int counter = 1 ; counter <= 10 ; counter++)
+{
+    cout << counter << endl;
+}
+
+cout << "akhir dari program";
+
+cin.get();
+return 0;
+}
---
+++ for_loop2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Awal program loop 2 \n";
+   for (int i = 1 ; i <= 200 ; i+=i)
+    {
+    cout << "angka";
+    cout << i << endl;
+   }
+   cout << "akhir dari program";
+
+   cin.get();
+   return 0;
+}
